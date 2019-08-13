1c67dd7b0f3dec2014b299282c8196f88089e718
Logon São Miguel <logon@cds.smg.lab>
2019-08-13 12:46:52 -0300

    Adicionar arquivos de projeto.

---
+++ AppBebidas/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace AppBebidas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.txtSabor = new System.Windows.Forms.TextBox();
+            this.txtSafra = new System.Windows.Forms.TextBox();
+            this.chkRetornavel = new System.Windows.Forms.CheckBox();
+            this.txtTipo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(41, 284);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 27);
+            this.btnInserir.TabIndex = 0;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(122, 284);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 27);
+            this.btnConsultar.TabIndex = 1;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(203, 284);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 27);
+            this.btnAlterar.TabIndex = 2;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(284, 284);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 27);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(147, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cadastro de Bebidas";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(99, 81);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(46, 20);
+            this.txtId.TabIndex = 5;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(99, 109);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(144, 20);
+            this.txtNome.TabIndex = 6;
+            this.txtNome.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(99, 137);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(97, 20);
+            this.txtValor.TabIndex = 7;
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Items.AddRange(new object[] {
+            "Refrigerante",
+            "Suco",
+            "Vinho"});
+            this.cmbCategoria.Location = new System.Drawing.Point(99, 165);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(121, 21);
+            this.cmbCategoria.TabIndex = 8;
+            this.cmbCategoria.SelectedIndexChanged += new System.EventHandler(this.cmbCategoria_SelectedIndexChanged);
+            // 
+            // txtSabor
+            // 
+            this.txtSabor.Enabled = false;
+            this.txtSabor.Location = new System.Drawing.Point(99, 219);
+            this.txtSabor.Name = "txtSabor";
+            this.txtSabor.Size = new System.Drawing.Size(97, 20);
+            this.txtSabor.TabIndex = 9;
+            // 
+            // txtSafra
+            // 
+            this.txtSafra.Enabled = false;
+            this.txtSafra.Location = new System.Drawing.Point(99, 247);
+            this.txtSafra.Name = "txtSafra";
+            this.txtSafra.Size = new System.Drawing.Size(97, 20);
+            this.txtSafra.TabIndex = 10;
+            // 
+            // chkRetornavel
+            // 
+            this.chkRetornavel.AutoSize = true;
+            this.chkRetornavel.Enabled = false;
+            this.chkRetornavel.Location = new System.Drawing.Point(99, 194);
+            this.chkRetornavel.Name = "chkRetornavel";
+            this.chkRetornavel.Size = new System.Drawing.Size(78, 17);
+            this.chkRetornavel.TabIndex = 11;
+            this.chkRetornavel.Text = "Retornável";
+            this.chkRetornavel.UseVisualStyleBackColor = true;
+            // 
+            // txtTipo
+            // 
+            this.txtTipo.Enabled = false;
+            this.txtTipo.Location = new System.Drawing.Point(249, 251);
+            this.txtTipo.Name = "txtTipo";
+            this.txtTipo.Size = new System.Drawing.Size(97, 20);
+            this.txtTipo.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Nome";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Valor";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Categoria";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(55, 224);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Sabor";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(58, 252);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Safra";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(215, 254);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Tipo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTipo);
+            this.Controls.Add(this.chkRetornavel);
+            this.Controls.Add(this.txtSafra);
+            this.Controls.Add(this.txtSabor);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnInserir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.TextBox txtSabor;
+        private System.Windows.Forms.TextBox txtSafra;
+        private System.Windows.Forms.CheckBox chkRetornavel;
+        private System.Windows.Forms.TextBox txtTipo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ AppBebidas/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppBebidas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbCategoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbCategoria.SelectedItem.ToString() == "Refrigerante")
+            {
+                chkRetornavel.Enabled = true;
+                txtSabor.Enabled = false;
+                txtSafra.Enabled = false;
+                txtTipo.Enabled = false;
+            }
+            else if (cmbCategoria.SelectedItem.ToString() == "Suco")
+            {
+                chkRetornavel.Enabled = false;
+                txtSabor.Enabled = true;
+                txtSafra.Enabled = false;
+                txtTipo.Enabled = false;
+            }
+            else if (cmbCategoria.SelectedItem.ToString() == "Vinho")
+            {
+                chkRetornavel.Enabled = false;
+                txtSabor.Enabled = false;
+                txtSafra.Enabled = true;
+                txtTipo.Enabled = true;
+            }
+        }
+    }
+}
